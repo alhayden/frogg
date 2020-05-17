0bec10d181de599c0ab0505ca0221d5436fba48a
zhayden14 <44064251+zhayden14@users.noreply.github.com>
2020-05-17 00:01:50 -0600

    documentation in ThreadCommand
    Improved documentation for commands in ThreadCommand used by NetworkThread

--- FroggThread/ThreadCommand.java
+++ FroggThread/ThreadCommand.java
@@ -1,32 +1,40 @@
 package FroggThread;
 
 public enum ThreadCommand{
-    //A ThreadCommand is sent as in integer and may be followed by a number of 
-    //integer data words
-
-    //replies for commands that require verification---------------------
+    //replies for commands that require verification----------------------------------------------------
     ACK,
     NACK,
     STATUS,
-    //general thread commands ---------------------------------------------
-    STOP, //stops execution of the thread (the thread should return as soon as possible)
-    SUSPEND,
-    RESUME,
-    //socket control commands----------------------------------------------
-    SET_MY_PORT,
-    SET_REMOTE_PORT,
-    SET_REMOTE_IP,
-    SET_SERVER,
-    SET_CLIENT,
+    //general thread commands --------------------------------------------------------------------------
+    STOP,               //stops execution of the thread (the thread should return as soon as possible)
+    SUSPEND,            //pauses execution of the thread (the thread should wait for RESUME)
+    RESUME,             //resumes execution of the thread
+    //socket control commands---------------------------------------------------------------------------
+    SET_MY_PORT,        //attempts to set local port to the specified port if disconnected
+                        //  responds: ACK or NACK, depending on success
+    SET_REMOTE_PORT,    //sets the remote port to the specified port if disconnected
+                        //  responds: ACK or NACK, depending on success
+    SET_REMOTE_IP,      //sets the remote IP to the selected IP if disconnected
+                        //  responds: ACK or NACK, depending on success
+    SET_SERVER,         //client: disconnects and changes mode to server
+                        //server: no effect
+    SET_CLIENT,         //client: no effect
+                        //server: disconnects all and changes mode to client
     CONNECT,            //client: tries to connect to given IP and port
+                        //  responds: ACK or NACK, depending on success
                         //server: allows the server to accept connections
+                        //  responds: ACK
     FREEZE,             //server only: stops server from accepting new connections
     DISCONNECT,         //client: disconnects from server   
+                        //  responds: ACK 
                         //server: tries to disconnect from the specified IP and port
-    GET_NET_STATUS,
-    //keyboard control commands
-    SET_KEYBIND,
+                        //  responds: ACK or NACK, depending on success
+    GET_NET_STATUS,     //gets status info from the network thread
+                        //  responds: STATUS, with network status info
+    //keyboard control commands--------------------------------------------------------------------------
+    SET_KEYBIND,//?
 
+    //catch-all invalid command
     INVALID;
 
 //     private final int code;
